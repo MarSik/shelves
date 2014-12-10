5311fe771b05a909eb9a739421a72d32edb07977
Martin Sivak <msivak@redhat.com>
2014-12-10 17:18:46 +0100

    Add drag'n'drop support component

---
+++ services/web/src/main/ember/app/components/draggable-dropzone.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    classNames: ['draggable-dropzone'],
+    classNameBindings: ['dragClass'],
+    dragClass: 'deactivated',
+
+    dragLeave: function(event) {
+        event.preventDefault();
+        this.set('dragClass', 'deactivated');
+    },
+
+    dragOver: function(event) {
+        event.preventDefault();
+        this.set('dragClass', 'activated');
+    },
+
+    drop: function(event) {
+        event.preventDefault();
+        this.set('dragClass', 'deactivated');
+        data = event.dataTransfer.getData('text/data');
+        // default drop action - change with {{draggable-dropzone dropped=xyz}}
+        this.sendAction('dropped', data);
+    }
+});
---
+++ services/web/src/main/ember/app/components/draggable-item.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    classNames: ['draggable-item'],
+    attributeBindings: ['draggable'],
+    draggable: 'true',
+
+    dragStart: function(event) {
+        event.dataTransfer.setData('text/data', this.get('content'))
+    }
+});
