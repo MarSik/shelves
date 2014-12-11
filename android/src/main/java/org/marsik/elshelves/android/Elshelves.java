74a8e3b1d6049a964ce5cd403a46d25a6b833600
Martin Sivak <mars@montik.net>
2014-12-11 20:00:26 +0100

    Use Android Annotations

--- android/src/main/java/org/marsik/elshelves/android/Elshelves.java
+++ android/src/main/java/org/marsik/elshelves/android/Elshelves.java
@@ -2,7 +2,9 @@ package org.marsik.elshelves.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import org.androidannotations.annotations.EActivity;
 
+@EActivity(R.layout.main)
 public class Elshelves extends Activity {
     /**
      * Called when the activity is first created.
