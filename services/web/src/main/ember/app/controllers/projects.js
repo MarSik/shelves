27b33e34a78f8b3ebad17c7b46b9fac42222d315
Martin Sivak <msivak@redhat.com>
2015-01-07 14:30:25 +0100

    Sort some lists and improve layout of requirements buttons

--- services/web/src/main/ember/app/controllers/projects.js
+++ services/web/src/main/ember/app/controllers/projects.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-    projectName: "New project"
+    projectName: "New project",
+    sortProperties: ['name'],
+    sortAscending: true
 });
