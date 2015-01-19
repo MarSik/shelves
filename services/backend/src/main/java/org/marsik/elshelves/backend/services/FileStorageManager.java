37d572e802c1bed7a949d667d80dba09147a41f1
Martin Sivak <mars@montik.net>
2015-01-19 21:16:08 +0100

    Implement sha256 hashing and hardlink deduplication in file store

--- services/backend/src/main/java/org/marsik/elshelves/backend/services/FileStorageManager.java
+++ services/backend/src/main/java/org/marsik/elshelves/backend/services/FileStorageManager.java
@@ -1,7 +1,10 @@
 package org.marsik.elshelves.backend.services;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.FileUtils;
 import org.marsik.elshelves.backend.configuration.StorageConfiguration;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -11,10 +14,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
+import java.nio.file.Files;
 import java.util.UUID;
 
 public class FileStorageManager implements StorageManager {
     final StorageConfiguration storageConfiguration;
+
+    final Object hashStoreLock = new Object();
 
     public FileStorageManager(StorageConfiguration storageConfiguration) {
         this.storageConfiguration = storageConfiguration;
@@ -25,6 +31,8 @@ public class FileStorageManager implements StorageManager {
         sb.append(storageConfiguration.getDocumentPath());
         sb.append("/");
         sb.append(uuid.toString().substring(0, 2));
+        sb.append("/");
+        sb.append(uuid.toString().substring(2, 4));
         sb.append("/");
         sb.append(uuid.toString());
         return sb.toString();
@@ -44,9 +52,23 @@ public class FileStorageManager implements StorageManager {
         return new FileOutputStream(f);
     }
 
+    public void notifyStored(UUID uuid) {
+        performHardlinking(uuid);
+    }
+
 	@Override
 	public void download(UUID uuid, URL url) throws IOException {
-		FileUtils.copyURLToFile(url, new File(getPath(uuid)));
+        File downloadTarget = new File(getPath(uuid) + ".download");
+
+        try {
+            FileUtils.copyURLToFile(url, downloadTarget);
+            FileUtils.moveFile(downloadTarget, get(uuid));
+        } catch (IOException ex) {
+            downloadTarget.delete();
+            return;
+        }
+
+        notifyStored(uuid);
 	}
 
 	@Override
@@ -65,4 +87,64 @@ public class FileStorageManager implements StorageManager {
 	public File get(UUID uuid) {
 		return new File(getPath(uuid));
 	}
+
+    private File getHashFile(String hash) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(storageConfiguration.getDocumentPath());
+        sb.append("/store/");
+        sb.append(hash.substring(0, 2));
+        sb.append("/");
+        sb.append(hash.substring(2, 4));
+        sb.append("/");
+        sb.append(hash);
+        return new File(sb.toString());
+    }
+
+    @Scheduled(cron = "* 15 4 * * *")
+    private void performHardLinkMaintenance() {
+        // Traverse checksum store and delete unused hardlinks
+    }
+
+    @Scheduled(cron = "* 15 3 * * *")
+    private void performDownloadMaintenance() {
+        // Traverse files and delete stale (more than a day old) downloads
+    }
+
+    @Async
+    private void performHardlinking(UUID uuid) {
+        // Compute file checksum
+        String hash;
+        try {
+            hash = DigestUtils.sha256Hex(retrieve(uuid));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return;
+        }
+
+        // Synchronize access to the checksum store
+        File hashFile;
+        synchronized (hashStoreLock) {
+            try {
+                // Store into checksum store or get the proper path
+                hashFile = getHashFile(hash);
+                if (!hashFile.exists()) {
+                    FileUtils.copyFile(get(uuid), hashFile);
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                return;
+            }
+        }
+
+        // Delete original file and create hardlink
+        synchronized (this) {
+            try {
+                get(uuid).delete();
+                Files.createLink(get(uuid).toPath(), hashFile.toPath());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                return;
+            }
+        }
+    }
 }
