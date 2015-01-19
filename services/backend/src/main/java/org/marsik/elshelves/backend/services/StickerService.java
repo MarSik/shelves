2f76a0a6a6ead9cd8bc1f9a609e928bede43822f
Martin Sivak <msivak@redhat.com>
2015-01-19 16:30:00 +0100

    Improve sticker generator - limit text amount, properly wrap lines

--- services/backend/src/main/java/org/marsik/elshelves/backend/services/StickerService.java
+++ services/backend/src/main/java/org/marsik/elshelves/backend/services/StickerService.java
@@ -94,6 +94,49 @@ public class StickerService {
         return (font.getFontDescriptor().getFontBoundingBox().getHeight() / 1000) * fontSize;
     }
 
+	private String getSingleLine(String text, float maxWidth, PDFont font, int fontSize, boolean nice) {
+		int maxTrim = text.length();
+		int minTrim = 0;
+		String result = text;
+
+		while (maxTrim >= minTrim) {
+			int trim = (minTrim + maxTrim) / 2;
+			String candidate = text.substring(0, trim);
+
+			try {
+				if (fontSize * (font.getStringWidth(candidate.trim()) / 1000) > maxWidth) {
+					// Too long, but already close to the boundary
+					// try increasing it (avoids rounding errors)
+					if (maxTrim <= trim) {
+						maxTrim--;
+						continue;
+					}
+
+					// Too long, try smaller next time
+					maxTrim = trim;
+				} else {
+					// Store the best result so far
+					result = candidate;
+
+					// Short enough, but already close to the boundary
+					// try increasing it (avoids rounding errors)
+					if (minTrim >= trim) {
+						minTrim++;
+						continue;
+					}
+
+					// Short enough, try larger next time
+					minTrim = trim;
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+				return result;
+			}
+		}
+
+		return result;
+	}
+
     public Result generateStickers(StickerSettings settings, List<StickerCapable> objects) throws IOException {
         PDDocument document = new PDDocument();
         // Coordinates = millimeters * PDPage.MM_TO_UNITS
@@ -108,11 +151,15 @@ public class StickerService {
         Float codeEdgeSize = MM_TO_UNITS * Math.min(settings.getStickerHeightMm() - 2*settings.getStickerTopMarginMm(),
                 settings.getStickerWidthMm()) - 2*settings.getStickerLeftMarginMm();
 
-        PDPageContentStream contentStream = null;
+		// Load unicode fonts
+		PDFont titleFont = PDType0Font.load(document, this.getClass().getResourceAsStream("/org/marsik/elshelves/backend/fonts/DejaVuSans-Bold.ttf"));
+		PDFont summaryFont = PDType0Font.load(document, this.getClass().getResourceAsStream("/org/marsik/elshelves/backend/fonts/DejaVuSans.ttf"));
 
-        // Load unicode fonts
-        PDFont titleFont = PDType0Font.load(document, this.getClass().getResourceAsStream("/org/marsik/elshelves/backend/fonts/DejaVuSans-Bold.ttf"));
-        PDFont summaryFont = PDType0Font.load(document, this.getClass().getResourceAsStream("/org/marsik/elshelves/backend/fonts/DejaVuSans.ttf"));
+		Float titleWidth = MM_TO_UNITS * (settings.getStickerWidthMm() - 2*settings.getStickerLeftMarginMm());
+		Float summaryWidth = (MM_TO_UNITS * (settings.getStickerWidthMm() - 2*settings.getStickerLeftMarginMm())) - codeEdgeSize;
+		Float summaryHeight = (MM_TO_UNITS * (settings.getStickerHeightMm() - 2*settings.getStickerTopMarginMm())) - 1.4f * getFontHeight(titleFont, 12);
+
+		PDPageContentStream contentStream = null;
 
         for (StickerCapable object: objects) {
             stickerCount++;
@@ -154,16 +201,34 @@ public class StickerService {
                 contentStream.beginText();
                 contentStream.setFont(titleFont, 12);
                 contentStream.newLineAtOffset(leftCoord, pageHeight - (topCoord + getFontHeight(titleFont, 12)));
-                contentStream.showText(object.getName());
+                contentStream.showText(getSingleLine(object.getName(), titleWidth, titleFont, 12, false));
                 contentStream.endText();
             }
 
             if (object.getSummary() != null) {
                 contentStream.beginText();
                 contentStream.setFont(summaryFont, 10);
+				contentStream.setLeading(getFontHeight(summaryFont, 10));
                 contentStream.newLineAtOffset(leftCoord + codeEdgeSize, pageHeight - (topCoord
-                        + 1.4f * getFontHeight(titleFont, 12) + getFontHeight(summaryFont, 10)));
-                contentStream.showText(object.getSummary());
+                        + getFontHeight(titleFont, 12) + getFontHeight(summaryFont, 10)));
+
+				int idx = 0;
+				int lines = (int)Math.floor(summaryHeight / (getFontHeight(summaryFont, 10)));
+
+				while (lines > 0 && idx < object.getSummary().length()) {
+					lines--;
+					String part = getSingleLine(object.getSummary().substring(idx),
+							summaryWidth,
+							summaryFont,
+							10,
+							true);
+					contentStream.showText(part.trim());
+					if (lines > 0) {
+						contentStream.newLine();
+					}
+					idx += part.length();
+				}
+
                 contentStream.endText();
             }
         }
