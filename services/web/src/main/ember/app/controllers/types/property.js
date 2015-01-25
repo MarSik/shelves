4f5969ff6a229a3737d7e7078dd5fce50a924b61
Martin Sivak <mars@montik.net>
2015-01-25 14:20:09 +0100

    Show properties using nice base

--- services/web/src/main/ember/app/controllers/types/property.js
+++ services/web/src/main/ember/app/controllers/types/property.js
@@ -6,16 +6,44 @@ export default Ember.ObjectController.extend({
     value: function() {
         var property = this.get('model');
         var entity = this.get('entity');
-        var res =  entity.get('values.'+property.get('id')) + " ";
 
-        if (!Ember.isEmpty(property.get('base.prefix'))) {
-            res += property.get('base.prefix');
+        // Base iso prefix exponent and value
+        var base = property.get('base.power10');
+        var baseValue = entity.get('values.'+property.get('id'));
+
+        // Nice value and prefix object
+        var niceValue = baseValue;
+        var nicePrefix = property.get('base.content');
+
+        var res =  " ";
+
+        if (!Ember.isEmpty(nicePrefix)) {
+            // Prefixes available for unit
+            var unit = property.get('unit.content');
+            var prefixes = unit.get('prefixes.content');
+
+            if (!Ember.isEmpty(prefixes)) {
+                prefixes.forEach(function (p) {
+                    var prefix = p.get('power10');
+                    var divider = prefix - base;
+                    var value = baseValue / Math.pow(10, divider);
+
+                    if (value > 0 && value.toString().length < niceValue.toString().length) {
+                        niceValue = value;
+                        nicePrefix = p;
+                    }
+                });
+            }
+
+            res += nicePrefix.get('prefix');
         }
+
+        res = niceValue + res;
 
         if (!Ember.isEmpty(property.get('unit.symbol'))) {
             res += property.get('unit.symbol');
         }
 
         return res;
-    }.property('model.base.prefix', 'model.unit.symbol', 'entity')
+    }.property('model.base.content.prefix', 'model.base.content.power10', 'model.unit.content.symbol', 'entity', 'model.unit.content.prefixes', 'model.unit.content.prefixes.@each.power10', 'model.unit.content.prefixes.@each.prefix')
 });
