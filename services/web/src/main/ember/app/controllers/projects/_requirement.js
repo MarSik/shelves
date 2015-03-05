03d96f705d3a4c34c5dfb09cfc527f2e0865dee2
Martin Sivak <msivak@redhat.com>
2015-03-05 18:34:21 +0100

    Compute assignable lots using promises

--- services/web/src/main/ember/app/controllers/projects/_requirement.js
+++ services/web/src/main/ember/app/controllers/projects/_requirement.js
@@ -4,32 +4,38 @@ export default Ember.ObjectController.extend({
     possibleTypes: Ember.computed.map('model.type', m => m),
     assignedLots: Ember.computed.map('model.lots', m => m),
 
-    candidateLots: function () {
+    candidateLots: function (key, value) {
+        if (arguments.length > 1) {
+            return value;
+        }
+
+        var self = this;
         var req = this.get('model');
 
         console.log('Recomputing available parts');
-
-        var usable = Ember.A();
 
         if (Ember.isEmpty(req)) {
             return usable;
         }
 
-        this.get('possibleTypes').forEach(function (type) {
-            console.log('type:');
-            console.log(type);
-            type.get('lots').then(function (ll) {
-                ll.forEach(function (lot) {
-                    console.log(lot);
-                    usable.pushObject(lot);
+        Promise.all(this.get('possibleTypes')).then(function (types) {
+            var lots = [];
+            types.forEach(function (type) {
+                lots.pushObject(type.get('lots'));
+            });
+
+            Promise.all(lots).then(function (lots) {
+                var usable = [];
+                lots.forEach(function (lotArray) {
+                    lotArray.forEach(function (lot) {
+                        usable.pushObject(lot);
+                    });
                 });
+                self.set('candidateLots', usable);
             });
         });
 
-        console.log("done");
-        console.log(usable);
-
-        return usable;
+        return [];
     }.property('possibleTypes.@each.lots'),
 
     assignableLots: Ember.computed.map('resolvedLots', m => m),
