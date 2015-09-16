51faedcbb4e675dd8c3bc976b41d7b06bc01365e
Martin Sivak <msivak@redhat.com>
2015-09-16 16:09:38 +0200

    Fix JPA entity location

--- services/backend/src/main/java/org/marsik/elshelves/backend/app/ApplicationData.java
+++ services/backend/src/main/java/org/marsik/elshelves/backend/app/ApplicationData.java
@@ -7,6 +7,7 @@ import org.marsik.elshelves.backend.services.FileStorageManager;
 import org.marsik.elshelves.backend.services.StorageManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -16,6 +17,7 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
 @EnableJpaRepositories("org.marsik.elshelves.backend.repositories")
+@EntityScan(basePackages = "org.marsik.elshelves.backend.entities")
 @EnableTransactionManagement
 public class ApplicationData {
     private static final Logger logger = LoggerFactory.getLogger(ApplicationData.class);
