67d54d3620b78730939183eaf692084b80e9303a
Martin Sivak <msivak@redhat.com>
2015-09-18 14:40:27 +0200

    Add necessary constructors to AuthorizationApiModel

--- lib/api/src/main/java/org/marsik/elshelves/api/entities/AuthorizationApiModel.java
+++ lib/api/src/main/java/org/marsik/elshelves/api/entities/AuthorizationApiModel.java
@@ -7,11 +7,20 @@ import lombok.EqualsAndHashCode;
 import org.marsik.elshelves.api.ember.EmberModelName;
 import org.marsik.elshelves.api.entities.idresolvers.AuthorizationIdResolver;
 
+import java.util.UUID;
+
 @EmberModelName("authorization")
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id", resolver = AuthorizationIdResolver.class)
 @Data
 @EqualsAndHashCode(of = {}, callSuper = true)
 public class AuthorizationApiModel extends AbstractEntityApiModel {
+    public AuthorizationApiModel(UUID id) {
+        super(id);
+    }
+
+    public AuthorizationApiModel() {
+    }
+
     String name;
     String secret;
 }
