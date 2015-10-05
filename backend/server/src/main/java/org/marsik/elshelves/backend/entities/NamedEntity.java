4d46a4eec296859e45bc641abc7c1bf5a3051f44
Martin Sivak <msivak@redhat.com>
2015-10-05 14:05:15 +0200

    Fix two missing imports

--- backend/server/src/main/java/org/marsik/elshelves/backend/entities/NamedEntity.java
+++ backend/server/src/main/java/org/marsik/elshelves/backend/entities/NamedEntity.java
@@ -19,6 +19,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
 import java.util.Set;
 
 @Data
--- backend/server/src/main/java/org/marsik/elshelves/backend/entities/NumericProperty.java
+++ backend/server/src/main/java/org/marsik/elshelves/backend/entities/NumericProperty.java
@@ -15,6 +15,7 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.Collection;
 
 @Data
