2583280b3b784731f93366045cbd72b51834234b
Martin Sivak <msivak@redhat.com>
2015-10-18 15:44:54 +0200

    Two level index support for adding items

--- web/src/main/ember/app/components/two-level-index.js
+++ web/src/main/ember/app/components/two-level-index.js
@@ -1,10 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    add(item) {
+      this.get('createAction')(item);
+    }
+  },
   tagName: "li",
   sorting: ['fullName'],
   sorted: Ember.computed.sort('model.children', 'sorting'),
 
+  createAction: undefined,
   model: undefined,
   target: undefined
 });
