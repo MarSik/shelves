157fb2b796393269612d67bd65c1ec3b73e3cfa7
Martin Sivak <msivak@redhat.com>
2015-10-20 22:18:28 +0200

    Fix property display for nested groups

--- web/src/main/ember/app/pods/components/group-type-line/component.js
+++ web/src/main/ember/app/pods/components/group-type-line/component.js
@@ -4,6 +4,7 @@ export default Ember.Component.extend({
   tagName: '',
   model: undefined,
   sorting: undefined,
+  showProperties: null,
   columnCount: null,
   sortedTypes: Ember.computed.sort('model.types', 'sorting')
 });
