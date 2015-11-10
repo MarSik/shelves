3c674961aabbc565b288bd7c94869a055200126c
Martin Sivak <msivak@redhat.com>
2015-11-10 23:11:03 +0100

    Add currency aware transaction editing

--- web/src/main/ember/app/pods/transactions/show/controller.js
+++ web/src/main/ember/app/pods/transactions/show/controller.js
@@ -6,6 +6,23 @@ export default Ember.Controller.extend({
         delivered: function () {
             console.log("delivering ..");
             return true;
+        },
+
+        editPrice(purchase) {
+            this.set('selectedItem', purchase);
+        },
+
+        updatePrice() {
+            var purchase = this.get('selectedItem');
+
+            purchase.set('singlePricePaid', this.get('exchangeRate') * purchase.get('singlePrice'));
+            purchase.set('totalPrice', purchase.get('count') * purchase.get('singlePrice'));
+            purchase.set('totalPricePaid', this.get('exchangeRate') * purchase.get('totalPrice'));
+
+            var self = this;
+            purchase.save().then(function () {
+              self.set('selectedItem', null);
+            });
         }
     },
     boxSorting: ['fullName'],
@@ -15,5 +32,17 @@ export default Ember.Controller.extend({
     location: null,
     editEnabled: Ember.computed('model.locked', function () {
         return !this.get('model.locked');
+    }),
+
+    selectedItem: null,
+    exchangeRate: 1.00,
+
+    selectedItemPaidPrice: Ember.computed('selectedItem', 'exchangeRate', function () {
+      var purchase = this.get('selectedItem');
+      var exchangeRate = this.get('exchangeRate');
+
+      if (Ember.isEmpty(purchase)) return null;
+
+      return purchase.get('singlePrice') * exchangeRate;
     })
 });
