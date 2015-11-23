ec4cf6812a55c8f2ab508f7c3eeb74b4234481c2
Martin Sivak <msivak@redhat.com>
2015-11-23 13:51:57 +0100

    Fix the StubSerializer and the jackson dependency

--- lib/api/src/main/java/org/marsik/elshelves/jackson/IdWhenStubSerializer.java
+++ lib/api/src/main/java/org/marsik/elshelves/jackson/IdWhenStubSerializer.java
@@ -3,20 +3,27 @@ package org.marsik.elshelves.jackson;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.BeanDescription;
+import com.fasterxml.jackson.databind.BeanProperty;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializationConfig;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatVisitable;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatVisitorWrapper;
+import com.fasterxml.jackson.databind.jsonschema.SchemaAware;
 import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
 import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
+import com.fasterxml.jackson.databind.ser.ContextualSerializer;
 import com.fasterxml.jackson.databind.ser.PropertyWriter;
+import com.fasterxml.jackson.databind.ser.ResolvableSerializer;
 import com.fasterxml.jackson.databind.ser.std.BeanSerializerBase;
 import com.fasterxml.jackson.databind.util.NameTransformer;
 import org.marsik.elshelves.api.entities.AbstractEntityApiModel;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.Iterator;
 
 public class IdWhenStubSerializer extends BeanSerializerModifier {
@@ -24,16 +31,17 @@ public class IdWhenStubSerializer extends BeanSerializerModifier {
     public JsonSerializer<?> modifySerializer(SerializationConfig config, BeanDescription beanDesc, JsonSerializer<?> serializer) {
         if (StubSupport.class.isAssignableFrom(beanDesc.getBeanClass())
                 && serializer instanceof BeanSerializerBase) {
-            return new StubSerializer((JsonSerializer<StubSupport>) serializer);
+            return new StubSerializer((BeanSerializerBase)serializer);
         }
 
         return super.modifySerializer(config, beanDesc, serializer);
     }
 
-    private static class StubSerializer extends JsonSerializer<StubSupport> {
-        final JsonSerializer<StubSupport> parent;
+    private static class StubSerializer extends JsonSerializer<StubSupport>
+            implements ContextualSerializer, ResolvableSerializer, SchemaAware {
+        final BeanSerializerBase parent;
 
-        public StubSerializer(JsonSerializer<StubSupport> parent) {
+        public StubSerializer(BeanSerializerBase parent) {
             this.parent = parent;
         }
 
@@ -46,6 +54,34 @@ public class IdWhenStubSerializer extends BeanSerializerModifier {
             } else {
                 parent.serialize(entity, jgen, provider);
             }
+        }
+
+        @Override
+        public JsonSerializer<?> createContextual(SerializerProvider serializerProvider, BeanProperty beanProperty)
+                throws JsonMappingException {
+            JsonSerializer<?> parentSerializer = parent.createContextual(serializerProvider, beanProperty);
+            if (parentSerializer instanceof BeanSerializerBase) {
+                return new StubSerializer((BeanSerializerBase)parentSerializer);
+            } else {
+                return parentSerializer;
+            }
+        }
+
+        @Override
+        public void resolve(SerializerProvider serializerProvider) throws JsonMappingException {
+            parent.resolve(serializerProvider);
+        }
+
+        @Override
+        public JsonNode getSchema(SerializerProvider serializerProvider, Type type)
+                throws JsonMappingException {
+            return parent.getSchema(serializerProvider, type);
+        }
+
+        @Override
+        public JsonNode getSchema(SerializerProvider serializerProvider, Type type, boolean b)
+                throws JsonMappingException {
+            return parent.getSchema(serializerProvider, type, b);
         }
     }
 }
