1dda4b7aa88c10c904381003607e3fcb212ea6d1
Martin Sivak <msivak@redhat.com>
2016-03-28 22:16:55 +0200

    Use common roles repository for ansible examples

--- doc/docker/shelves-backend/run.sh
+++ doc/docker/shelves-backend/run.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 
-export DIR_CONFIG="/etc/shelves"
-export DEPLOY="/server.war"
-export LOG_FILE="/shelves/log/shelves.log"
+DIR_CONFIG="/etc/shelves"
+DEPLOY="/server.war"
+DIR_LOG="/shelves/log"
+LOG_FILE="$DIR_LOG/shelves.log"
+DIR_DATA="/shelves/data"
+
+umask 0077
+mkdir -m 0700 -p $DIR_LOG
+mkdir -m 0700 -p $DIR_DATA
 
 umask 0077
 mkdir -m 0700 -p $DIR_DATA
 
-JAVA_OPTS="-Xms128m -Xmx3072m -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=127.0.0.1:8000,suspend=n"
+JAVA_OPTS="-Xms$JAVA_MIN_MEM -Xmx$JAVA_MAX_MEM -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=0.0.0.0:8000,suspend=n"
 MAINCLASS=$(grep "Main-Class: " "$DEPLOY/META-INF/MANIFEST.MF" | cut -d ' ' -f2 | sed -e 's/[[:space:]]//g')
 
-exec java $JAVA_OPTS -cp "$DEPLOY" -Dlogging.config=$DIR_CONFIG/logback.xml -Ddebug -Dserver.port=8080 -Dspring.config.location=$DIR_CONFIG/ $MAINCLASS "$@"
+exec java $JAVA_OPTS -cp "$DEPLOY" \
+-Dlogging.config=$DIR_CONFIG/logback.xml \
+-Ddebug \
+-Dserver.port=8080 \
+-Dspring.config.location=$DIR_CONFIG/ \
+-Dstorage.path=$DIR_DATA \
+"-Dshelves.memcached.server=$MEMCACHED_SERVER" \
+"-Dspring.datasource.url=jdbc:mysql://$MYSQL_SERVER/$MYSQL_DATABASE?useUnicode=yes&characterEncoding=UTF-8" \
+$MAINCLASS "$@"
 
