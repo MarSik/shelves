ae1f3d591bec86667eb050fd49aa5516cf2a747f
Martin Sivak <msivak@redhat.com>
2016-04-01 16:14:40 +0200

    Obey config.js in the oauth client configuration

--- web/src/main/ember/app/initializers/server-setup.js
+++ web/src/main/ember/app/initializers/server-setup.js
@@ -3,11 +3,16 @@ import Ember from 'ember';
 
 export default {
     name: 'api-server-setup',
+    before: ['oauth2-w-auth'],
+
     initialize: function() {
         if (!Ember.isEmpty(window.SHELVES_API_SERVER)) {
              ENV.APP.API_SERVER = window.SHELVES_API_SERVER;
              ENV.APP.API_BASE = window.SHELVES_API_BASE;
              ENV.APP.API_NAMESPACE = ENV.APP.API_BASE + window.SHELVES_API_NAMESPACE;
+
+             ENV['simple-auth-oauth2'].serverTokenEndpoint = ENV.APP.API_SERVER
+                 + (ENV.APP.API_BASE ? '/' + ENV.APP.API_BASE : '') + '/oauth/token';
         }
     }
 };
