550da0ff01309ff75dfec3914c5d078c64ef67a0
Martin Sivak <msivak@redhat.com>
2016-04-04 23:00:03 +0200

    [webapp] Update crossOriginWhitelist properly when using config.js

--- web/src/main/ember/app/initializers/server-setup.js
+++ web/src/main/ember/app/initializers/server-setup.js
@@ -11,6 +11,7 @@ export default {
              ENV.APP.API_BASE = window.SHELVES_API_BASE;
              ENV.APP.API_NAMESPACE = ENV.APP.API_BASE + window.SHELVES_API_NAMESPACE;
 
+             ENV['simple-auth'].crossOriginWhitelist = [ENV.APP.API_SERVER];
              ENV['simple-auth-oauth2'].serverTokenEndpoint = ENV.APP.API_SERVER
                  + (ENV.APP.API_BASE ? '/' + ENV.APP.API_BASE : '') + '/oauth/token';
         }
