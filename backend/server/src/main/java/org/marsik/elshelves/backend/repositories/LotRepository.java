a0e865aba0df039218b0bc1558e2a29dc8c2530d
Martin Sivak <msivak@redhat.com>
2016-04-07 22:48:40 +0200

    Fix part count metric

--- backend/server/src/main/java/org/marsik/elshelves/backend/repositories/LotRepository.java
+++ backend/server/src/main/java/org/marsik/elshelves/backend/repositories/LotRepository.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface LotRepository extends BaseOwnedEntityRepository<Lot> {
     @Query("SELECT NEW org.marsik.elshelves.backend.repositories.results.LotMetric(" +
-            " count(l.id)," +
-            " SUM(CASE l.used WHEN true THEN 1 ELSE 0 END)," +
-            " SUM(CASE l.usedInPast WHEN true THEN 1 ELSE 0 END)" +
+            " SUM(l.count)," +
+            " SUM(CASE l.used WHEN true THEN l.count ELSE 0 END)," +
+            " SUM(CASE l.usedInPast WHEN true THEN l.count ELSE 0 END)" +
             " ) " +
             " FROM Lot l" +
             " WHERE l.valid = true")
