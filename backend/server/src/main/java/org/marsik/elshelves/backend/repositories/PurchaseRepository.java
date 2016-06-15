e7b52c835e512cdfd5ac79c1bbbb12e66b42994c
Martin Sivak <msivak@redhat.com>
2016-06-15 23:48:13 +0200

    Implement DAO test for PurchaseRepository.findUndelivered

--- backend/server/src/main/java/org/marsik/elshelves/backend/repositories/PurchaseRepository.java
+++ backend/server/src/main/java/org/marsik/elshelves/backend/repositories/PurchaseRepository.java
@@ -1,5 +1,7 @@
 package org.marsik.elshelves.backend.repositories;
 
+import java.util.UUID;
+
 import org.marsik.elshelves.backend.entities.Purchase;
 import org.marsik.elshelves.backend.entities.Type;
 import org.marsik.elshelves.backend.entities.User;
@@ -8,6 +10,6 @@ import org.springframework.data.jpa.repository.Query;
 public interface PurchaseRepository extends BaseIdentifiedEntityRepository<Purchase> {
 	Iterable<Purchase> findByTransactionOwner(User owner);
 
-	@Query("SELECT p FROM OwnedEntity o, Purchase p, Lot l0, PurchasedLot l WHERE o.dbId = l.dbId AND o.owner = ?1 AND l.purchase = p AND l0.dbId = l.dbId AND l0.type = ?2 AND l0.valid = true GROUP BY p HAVING sum(l0.count) < p.count")
-	Iterable<Purchase> findUndelivered(User owner, Type type);
+	@Query("SELECT p.id FROM Purchase p, PurchasedLot l WHERE p.owner = ?1 AND l.purchase = p AND l.type = ?2 AND l.valid = true GROUP BY p.id, p.count HAVING sum(l.count) < p.count")
+	Iterable<UUID> findUndelivered(User owner, Type type);
 }
---
+++ backend/server/src/test/java/org/marsik/elshelves/backend/test/dao/PurchaseDaoTest.java
@@ -0,0 +1,146 @@
+package org.marsik.elshelves.backend.test.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+import org.marsik.elshelves.api.entities.fields.LotAction;
+import org.marsik.elshelves.backend.entities.LotHistory;
+import org.marsik.elshelves.backend.entities.Purchase;
+import org.marsik.elshelves.backend.entities.PurchasedLot;
+import org.marsik.elshelves.backend.entities.Source;
+import org.marsik.elshelves.backend.entities.Transaction;
+import org.marsik.elshelves.backend.entities.Type;
+import org.marsik.elshelves.backend.entities.User;
+import org.marsik.elshelves.backend.repositories.LotHistoryRepository;
+import org.marsik.elshelves.backend.repositories.LotRepository;
+import org.marsik.elshelves.backend.repositories.PurchaseRepository;
+import org.marsik.elshelves.backend.repositories.SourceRepository;
+import org.marsik.elshelves.backend.repositories.TransactionRepository;
+import org.marsik.elshelves.backend.repositories.TypeRepository;
+import org.marsik.elshelves.backend.repositories.UserRepository;
+import org.marsik.elshelves.backend.services.UuidGenerator;
+import org.marsik.elshelves.backend.services.UuidGeneratorImpl;
+import org.mockito.Spy;
+import org.modelmapper.internal.util.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+public class PurchaseDaoTest extends BaseDaoTest {
+    @Autowired
+    PurchaseRepository purchaseRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    TypeRepository typeRepository;
+
+    @Autowired
+    TransactionRepository transactionRepository;
+
+    @Autowired
+    LotRepository lotRepository;
+
+    @Autowired
+    SourceRepository sourceRepository;
+
+    @Autowired
+    LotHistoryRepository lotHistoryRepository;
+
+    @Spy
+    UuidGenerator uuidGenerator = new UuidGeneratorImpl();
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testExpectedDelivery() {
+        UUID uuid = uuidGenerator.generate();
+        User user = new User();
+        user.setName("Test user");
+        user.setEmail("user@test.com");
+        user.setId(uuid);
+        user.setCreated(new DateTime());
+        user.setLastModified(new DateTime());
+
+        userRepository.save(user);
+
+        Type type = new Type();
+        type.setName("Type");
+        type.setId(uuidGenerator.generate());
+        type.setOwner(user);
+
+        typeRepository.save(type);
+
+        Source source = new Source();
+        source.setName("source");
+        source.setId(uuidGenerator.generate());
+        source.setOwner(user);
+
+        sourceRepository.save(source);
+
+        Transaction transaction = new Transaction();
+        transaction.setId(uuidGenerator.generate());
+        transaction.setName("transaction");
+        transaction.setOwner(user);
+        transaction.setSource(source);
+
+        transactionRepository.save(transaction);
+
+        Purchase purchase = new Purchase();
+        purchase.setId(uuidGenerator.generate());
+        purchase.setType(type);
+        purchase.setCount(20L);
+        purchase.setOwner(user);
+        purchase.setTransaction(transaction);
+
+        purchaseRepository.save(purchase);
+
+        LotHistory lotHistory = new LotHistory();
+        lotHistory.setId(uuidGenerator.generate());
+        lotHistory.setValidSince(DateTime.now());
+        lotHistory.setAction(LotAction.ASSIGNED);
+
+        lotHistoryRepository.save(lotHistory);
+
+        PurchasedLot lot = new PurchasedLot();
+        lot.setId(uuidGenerator.generate());
+        lot.setCount(10L);
+        lot.setValid(true);
+        lot.setType(type);
+        lot.setPurchase(purchase);
+        lot.setOwner(user);
+        lot.setHistory(lotHistory);
+
+        lotRepository.save(lot);
+
+        Purchase purchase2 = new Purchase();
+        purchase2.setId(uuidGenerator.generate());
+        purchase2.setType(type);
+        purchase2.setCount(20L);
+        purchase2.setOwner(user);
+        purchase2.setTransaction(transaction);
+
+        purchaseRepository.save(purchase2);
+
+        PurchasedLot lot2 = new PurchasedLot();
+        lot2.setId(uuidGenerator.generate());
+        lot2.setCount(20L);
+        lot2.setValid(true);
+        lot2.setType(type);
+        lot2.setPurchase(purchase2);
+        lot2.setOwner(user);
+        lot2.setHistory(lotHistory);
+
+        lotRepository.save(lot2);
+
+        List<UUID> ids = Lists.from(purchaseRepository.findUndelivered(user, type).iterator());
+        assertThat(ids)
+                .hasSize(1)
+                .contains(purchase.getId());
+    }
+}
