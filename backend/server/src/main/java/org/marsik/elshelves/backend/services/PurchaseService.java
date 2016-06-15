44fc3ffcae682935bd36657ce65fb802033d2957
Martin Sivak <msivak@redhat.com>
2016-06-16 00:32:53 +0200

    Add /types/<id>/undelivered endpoint to list purchases with missing lots

--- backend/server/src/main/java/org/marsik/elshelves/backend/services/PurchaseService.java
+++ backend/server/src/main/java/org/marsik/elshelves/backend/services/PurchaseService.java
@@ -1,9 +1,15 @@
 package org.marsik.elshelves.backend.services;
 
+import java.util.Collection;
+
 import org.marsik.elshelves.backend.controllers.exceptions.OperationNotPermitted;
 import org.marsik.elshelves.backend.entities.Purchase;
+import org.marsik.elshelves.backend.entities.Type;
+import org.marsik.elshelves.backend.entities.User;
 import org.marsik.elshelves.backend.repositories.PurchaseRepository;
 
 public interface PurchaseService extends AbstractRestServiceIntf<PurchaseRepository, Purchase> {
     void deleteEntity(Purchase entity) throws OperationNotPermitted;
+
+    Collection<Purchase> findUndelivered(User user, Type type);
 }
