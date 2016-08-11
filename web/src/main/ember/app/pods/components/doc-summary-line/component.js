492fb9589e23e290ff60dbfc354662b98440eaed
Martin Sivak <mars@montik.net>
2016-08-11 15:59:48 +0200

    Prevent deletion of document that describes multiple entities

--- web/src/main/ember/app/pods/components/doc-summary-line/component.js
+++ web/src/main/ember/app/pods/components/doc-summary-line/component.js
@@ -1,9 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  growl: Ember.inject.service(),
   actions: {
     deleteDocument(doc) {
-      this.sendAction("deleteDocument", doc);
+      if (doc.get("describes.length") == 1) this.sendAction("deleteDocument", doc);
+      else this.get('growl').error("This document describes multiple entities. Please use the detail link to see them first.");
     }
   },
   tagName: 'tbody'
