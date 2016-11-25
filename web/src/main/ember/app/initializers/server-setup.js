b70ba6b9867a0b39a782139da2bbcedc7c8c35a0
Martin Sivak <msivak@redhat.com>
2016-11-25 17:32:43 +0100

    Update the server setup initializer

--- web/src/main/ember/app/initializers/server-setup.js
+++ web/src/main/ember/app/initializers/server-setup.js
@@ -1,16 +1,16 @@
 import ENV from 'webapp/config/environment';
 import Ember from 'ember';
 
+export function initialize(application) {
+  if (!Ember.isEmpty(window.SHELVES_API_SERVER)) {
+    ENV.APP.API_SERVER = window.SHELVES_API_SERVER;
+    ENV.APP.API_BASE = window.SHELVES_API_BASE;
+    ENV.APP.API_NAMESPACE = ENV.APP.API_BASE + window.SHELVES_API_NAMESPACE;
+    ENV.APP.API_ENDPOINT = ENV.APP.API_SERVER + (ENV.APP.API_NAMESPACE ? '/' + ENV.APP.API_NAMESPACE : '');
+  }
+};
+
 export default {
     name: 'api-server-setup',
-    before: ['ember-data'],
-
-    initialize: function(application) {
-        if (!Ember.isEmpty(window.SHELVES_API_SERVER)) {
-             ENV.APP.API_SERVER = window.SHELVES_API_SERVER;
-             ENV.APP.API_BASE = window.SHELVES_API_BASE;
-             ENV.APP.API_NAMESPACE = ENV.APP.API_BASE + window.SHELVES_API_NAMESPACE;
-             ENV.APP.API_ENDPOINT = ENV.APP.API_SERVER + (ENV.APP.API_NAMESPACE ? '/' + ENV.APP.API_NAMESPACE : '');
-        }
-    }
+    before: 'ember-data'
 };
